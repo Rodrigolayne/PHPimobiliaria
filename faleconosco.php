ebfadf162b0519255c442539bb7d7572ed9c771e
Rodrigolayne <30937521+Rodrigolayne@users.noreply.github.com>
2017-08-11 16:59:31 -0300

    Add files via upload

---
+++ faleconosco.php
@@ -0,0 +1,20 @@
+<?php include "inc/topo.php"; ?>
+
+<html> 
+<head>
+</head>
+<li><a href="index.php">Voltar</a></li>
+
+<body bgcolor = "#CCCCCC">
+
+<h2>Email: xampsonimoveis@gmail.com </h2>
+<h2>Telefone: (11)2725-5469</h2>
+<h2>Celular: (11)95136-8585</h2>
+<h2>Endereço da Imobiliária: Avenida Cipriano Rodrigues,665</h2>
+
+
+ 
+</body>
+</html>
+
+<?php include "inc/rodape.php"; ?>	
---
+++ quemsomos.php
@@ -0,0 +1,31 @@
+<?php include "inc/topo.php"; ?>
+<html>
+	<head>
+		<title>Nossa Empresa</title>
+	</head>
+		<li><a href="index.php">Voltar</a></li>
+		
+		
+	<body bgcolor="#CCCCCC">
+		<h2> <font color = "black">Nossa Empresa</h2>
+		
+
+			
+	<font size="5px">	A Xampson Imóveis é a mais nova imobiliária da cidade de São Paulo unimos modernidade e transformamos sonhos em realizações.
+		Fundada em 2016 pelo Sr. Maikon Xampsons, a imobiliária teve seu nome inspirado em seu sobrenome. 
+Com seu crescimento administrativo, a empresa passou a contar com o apoio familiar, tornando o atendimento mais humano.
+Expandindo-se por toda capital paulistana resultou na abertura de uma filial no Centro da cidade. inovando mais uma vez, 
+a imobiliária criou uma nova divisão focada em imóveis de alto padrão, a Nova São Paulo Gold.
+Estar na Imobiliária Nova São Paulo é sentir-se em casa. Aqui, diretores, colaboradores e parceiros têm o mesmo objetivo:
+ transformar os sonhos dos clientes em realizações. Por isso criamos relacionamentos tão duradouros.
+Valorizamos nossa tradição e investimos constantemente na modernização de ferramentas administrativas e na qualificação da nossa equipe.
+Parcerias fazem parte do nosso DNA. 
+A Nova São Paulo é associada à Rede Secovi de Imóveis e certificada como empresa Correspondente pela Caixa Econômica Federal (CEF).
+Venha nos visitar e descubra por que somos uma das mais tradicionais imobiliárias do centro de São Paulo.</font>
+	
+<!-- <center> <img class="templateml" src="http://i.imgur.com/10oyxe5.jpg" alt="" width="833" height="39" /></span></p> -->
+
+	</body>
+</html>
+
+<?php include "inc/rodape.php"; ?>	
